74bc8901febffa697056673e485eb84a7b10ce88
MarkZuckerbergStalker <98890647+MarkZuckerbergStalker@users.noreply.github.com>
2022-02-03 23:20:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ client = commands.Bot(command_prefix = '!', intents=discord.Intents.all())
 for i in range(len(cogs)):
   cogs[i].setup(client)
 
-client.run("OTM4ODQ5MTM3MDQwOTQ5Mjg4.YfwRZQ.z90C0g7tuYqQCJjIfIRvIgixDAw")
+client.run("OTM4ODQ5MTM3MDQwOTQ5Mjg4.YfwRZQ.1lPrwBOvXAakmkNnqkrCntDtjw4")
