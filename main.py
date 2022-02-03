f1a52c807f5d73408a9b22e6c1afdca18df9d46f
MarkZuckerbergStalker <98890647+MarkZuckerbergStalker@users.noreply.github.com>
2022-02-03 23:39:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ client = commands.Bot(command_prefix = '!', intents=discord.Intents.all())
 for i in range(len(cogs)):
   cogs[i].setup(client)
 
-client.run('BOTTOKEN')
+client.run(os.getenv('BOTTOKEN'))
